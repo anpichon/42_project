8685f56cf1975684d0f9c7c7e4e9f27aa3b7bf5f
Anthony PICHON <anpichon@e1r12p14.42.fr>
2016-10-28 11:27:40 +0200

    add ft_getnbr and pars

--- Fdf/fdf_pars.c
+++ Fdf/fdf_pars.c
@@ -3,11 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   fdf_pars.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anpichon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: anpichon <anpichon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/05 07:26:13 by anpichon          #+#    #+#             */
-/*   Updated: 2016/10/05 07:26:15 by anpichon         ###   ########.fr       */
+/*   Updated: 2016/10/28 11:26:21 by anpichon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	    ft_countreadfile(char *file)
+{
+  int		i;
+	int		fd;
+	char	*c;
 
+	if (!(fd = open(file, O_RDONLY)))
+			return (0);
+	i = 0;
+	while (get_next_line(fd, &c) > 0)
+		i++;
+	close(fd);
+	return (i);
+}
+
+int   ft_check_fdf(int fd, t_fdf *e)
+{
+
+}
